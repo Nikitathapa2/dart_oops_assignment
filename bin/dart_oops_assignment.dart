b15d364ab7b6a1e9672b49be1c97bebc60cbf44f
Nikita Thapa <thapanikita456@gmail.com>
2025-11-07 11:37:04 +0545

    first commit

---
+++ bin/dart_oops_assignment.dart
@@ -0,0 +1,5 @@
+import 'package:dart_oops_assignment/dart_oops_assignment.dart' as dart_oops_assignment;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_oops_assignment.calculate()}!');
+}
